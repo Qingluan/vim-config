d911f029f85de5d9ebd6d641d7401b2122e42873
dr <dr@mail.com>
2019-02-18 12:58:33 +0800

    fix

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
 cd ~/.vim/bundle/YouCompleteMe/
-apt install -y  cmake python3-dev gcc g++
+apt install -y build-essential clang cmake python3-dev gcc g++
 export CC=/usr/bin/gcc
 export CXX=/usr/bin/g++
 git submodule update --init --recursive
