6b7942635a698a2cfee23e24e85aaf5cc35bbd93
dr <dr@mail.com>
2019-02-18 12:07:32 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 git clone https://github.com/Qingluan/vim-config.git /tmp/vim-config && cd /tmp/vim-config
 
 mv vimrc ~/.vimrc 
-mv vim ~/.vim
+mkdir -p ~/.vim/bundle
 
-git submodule update --init --recursive
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~/.vim/bundle/YouCompleteMe 
+git clone https://github.com/Valloric/YouCompleteMe
 apt install -y  cmake python3-dev
-
+git submodule update --init --recursive
 python3 install.py 
 
+vim -c ":PluginInstall"
