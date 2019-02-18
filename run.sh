bcd493b6dc0d6a9e0ec2fccec93dcf7fd0264949
dr <dr@mail.com>
2019-02-18 12:11:39 +0800

    s

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ mv vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-cd ~/.vim/bundle/YouCompleteMe 
-git clone https://github.com/Valloric/YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe ~/.vim/bundle/YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe/
 apt install -y  cmake python3-dev
 git submodule update --init --recursive
 python3 install.py 
